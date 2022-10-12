24e329d0fb5015a7b41de116d01cec515a8be754
avi36856 <115415232+avi36856@users.noreply.github.com>
2022-10-12 12:54:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,14 +32,14 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.owner = ['94772836332'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['94772836332'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94772836332'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = '☆•Avi•☆' // Your name ඔබේ නම 
+global.botnma = '☆•Avi•☆' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = '☆•Avi•☆ ' //ownernama,ownername
+global.packname = '☆•Avi•☆ ' // Sticker package name 
+global.author = '☆•Avi•☆ ' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
